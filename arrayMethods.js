c1e0b13432740073063c4cda999596e284cbd2a0
Gabriel Simek <gabrielsimek@outlook.com>
2021-06-08 16:35:19 -0700

    arrayMap initial pass

---
+++ arrayMethods.js
@@ -0,0 +1,9 @@
+ 
+export function arrayMap(array, callback) {
+  const newArray = [];
+  for(let i = 0; i < array.length; i++){
+    newArray[i] = callback(array[i]);
+  }
+  return newArray;
+}
+  
--- arrayMethods.test.js
+++ arrayMethods.test.js
@@ -1,13 +1,12 @@
-
+import { arrayMap } from './arrayMethods.js';
 describe('arrayMap', () => {
   it('applies a function on each element in an array, returning a new array', () => {
     const array = [255, 0, 1, 'a', 'b'];
-    const newArray = arrayMap(array, item => {
+    const typesArray = arrayMap(array, item => {
       return typeof(item);
     });
     
-
     const expected = ['number', 'number', 'number', 'string', 'string'];
-    expect(newArray).toEqual(expected);
+    expect(typesArray).toEqual(expected);
   });
 });
